5ca80292cbd071bb3fbccfb130df8b557ec3ed66
test <test@gmail.com>
2024-10-21 08:50:21 +0700

    Fix bug

--- views/header.php
+++ views/header.php
@@ -20,7 +20,7 @@ if(!empty($_GET['keyword'])) {
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="list_users.php">App Web 1</a>
+                <a class="navbar-brand" href="list_users.php">App 1</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -43,10 +43,14 @@ if(!empty($_GET['keyword'])) {
                             Account <span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu">
+                            <?php if (!empty($id)){?>
                             <li><a href="view_user.php?id=<?php echo $id ?>">Profile</a></li>
                             <li role="separator" class="divider"></li>
-                            <li><a href="login.php">Login</a></li>
                             <li><a href="logout.php">Logout</a></li>
+                            <?php } else {?>
+                                <li><a href="login.php">Login</a></li>
+                            <?php }?>
+
                         </ul>
                     </li>
                 </ul>
